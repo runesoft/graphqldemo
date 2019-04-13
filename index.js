3bd24b6f463c4f1e7f6e6e79d8613953194030d8
Rune Juhl-Petersen <rune@juhl-petersen.net>
2019-04-13 19:47:38 +0200

    Mutate

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const user = {
+    name: "Bob",
+    age:  22
+};
+
 const typeDefs = gql`
 type Query{
     hello: String!
-    user: User
+    me: User
 }
 type User{
     name: String!
     age: Int!
+    mutation: String
 }
+type Mutation {
+        me(mutation: String!): User
+    }
 `;
 
 const resolvers = {
     Query: {
         hello: ()=>"asdf",
-        user: () => ({
-            name: "Someonse",
-            age: 23 
-        })
+        me: () => user
+    },
+    Mutation: {
+        me: (parent, args) => {
+            console.log(args);
+            if('mutation' in args){
+                user.mutation = args.mutation;
+            }
+            return user;
+        }
     }
+
 };
 
 const server = new ApolloServer({ typeDefs, resolvers});
