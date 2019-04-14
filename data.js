7f382ebf7530b7b7786f47143f4b1bcb197b339e
Rune Juhl-Petersen <rune@juhl-petersen.net>
2019-04-14 17:48:36 +0200

    features

---
+++ data.js
@@ -0,0 +1,26 @@
+module.exports.stories=[
+    {id: 1,     title: "Story 1",   status: "InProgress",   featureid: 1},
+    {id: 2,     title: "Story 2",   status: "InProgress",   featureid: 1},  
+    {id: 3,     title: "Story 3",   status: "InProgress",   featureid: 1},
+    {id: 4,     title: "Story 4",   status: "InProgress",   featureid: 2},
+    {id: 5,     title: "Story 5",   status: "InProgress",   featureid: 2},
+    {id: 6,     title: "Story 6",   status: "Done",         featureid: 3},
+    {id: 7,     title: "Story 7",   status: "InProgress"},
+];
+
+
+module.exports.features=[
+    {id: 1,     title: "Feature 1",   status: "InProgress",     epicid: 1},    
+    {id: 2,     title: "Feature 2",   status: "InProgress",     epicid: 1},    
+    {id: 3,     title: "Feature 3",   status: "InProgress",     epicid: 1},    
+    {id: 4,     title: "Feature 4",   status: "InProgress",     epicid: 1},    
+    {id: 5,     title: "Feature 5",   status: "InProgress",     epicid: 2},    
+
+];
+
+module.exports.epics=[
+    {id: 1,     title: "Epic 1",   status: "InProgress",     epicid: 1},    
+    {id: 2,     title: "Epic 2",   status: "InProgress",     epicid: 2},    
+    {id: 3,     title: "Epic 3",   status: "Refinement",     epicid: 3},    
+
+];
--- index.js
+++ index.js
@@ -1,9 +1,40 @@
+
+
 const { ApolloServer, gql } = require("apollo-server");
+const {stories, features, epics} = require('./data.js');
 
 const typeDefs = gql`
 type Query{
     hello: String!
     user: User
+    feature(id: ID): Feature
+    story(id: ID): Story
+    epic(id: ID): Epic
+}
+enum Status{
+    InProgress
+    Done
+    Accepted
+}
+interface WorkItem{
+    title: String!
+    status: Status!
+}
+type Story implements WorkItem{
+    feature: Feature
+    title: String!
+    status: Status!
+}
+type Feature implements WorkItem{
+    stories: [Story]
+    title: String!
+    status: Status!
+    epic: Epic
+}
+type Epic  implements WorkItem{
+    features: [Feature]
+    title: String!
+    status: Status!
 }
 type User{
     name: String!
@@ -11,13 +42,42 @@ type User{
 }
 `;
 
+
 const resolvers = {
     Query: {
         hello: ()=>"asdf",
         user: () => ({
             name: "Someonse",
             age: 23 
-        })
+        }),
+        epic: (args,{id}) => {
+            return epics.find(epic=>epic.id==id);
+        },
+        feature: (args,{id}) => {
+            return features.find(f=>f.id==id);
+        },
+        story: (args,{id})=>{
+            console.log(id);
+            return stories.find(s=>s.id==id);
+        },
+    },
+    Feature:{
+        stories: (feature)=>{
+            return stories.filter(s=>s.featureid===feature.id);
+        },
+        epic: (feature)=>{
+            return epics.find(e=>e.id===feature.epicid);
+        }
+    },
+    Story: {
+        feature: (story)=>{return features.find(f=>f.id==story.featureid);}
+    },
+    Epic: {
+        features: epic=>features.filter(feature=>feature.epicid==epic.id)
+    },
+    WorkItem: {
+        title: ()=>"a work item",
+        status: ()=>"Done"
     }
 };
 
